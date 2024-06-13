03c8a55bf61c94d3496d78a74d21114313b9f6ca
Albin Hedberg Abrahamsson <143542598+albin-hedberg@users.noreply.github.com>
2024-06-13 11:24:46 +0200

    Voxel-Terrain

--- Voxel-Terrain/Voxel-Terrain.cpp
+++ Voxel-Terrain/Voxel-Terrain.cpp
@@ -1,19 +1,17 @@
-﻿#define SDL_MAIN_HANDLED	// Because fucking SDL.h redefines main() and fucks with compilation! >:(		-> "SDL2main.lib(SDL_windows_main.obj) : error LNK2019..."
-// "Redefine main() on some platforms so that it is called by SDL."
+﻿#define SDL_MAIN_HANDLED	// "SDL2main.lib(SDL_windows_main.obj) : error LNK2019..."
 
 #include <SDL.h>
-//#undef main				// Possible alternative solution to: #define SDL_MAIN_HANDLED	(ta bort)
 #include <SDL_image.h>
 #include <iostream>
 #include <string>
 #include <math.h>
 #include <glad/glad.h>
 
-#define WINDOW_W 800	// Display resolution 1280
-#define WINDOW_H 600	// Display resolution 720
+#define WINDOW_W 800	// Display resolution
+#define WINDOW_H 600	// Display resolution
 
-#define SCALE_FACTOR 200	// Used for rendering Projection
-#define CAM_POS_X 512		// Camera starting pos
+#define SCALE_FACTOR 200	// Används till rendering projection
+#define CAM_POS_X 512
 #define CAM_POS_Y 512
 #define RENDER_DISTANCE 2000
 #define CAM_HEIGHT 150
@@ -21,10 +19,10 @@
 #define PI 3.141592
 
 #define MAP_N 1024		// Map resolution (1024x1024)
-#define MAP_SHIFT 2		// 2 images / Map (Color & Height)
+#define MAP_SHIFT 2		// 2 bilder / karta (Color & Height)
 
 #define FPS 60
-#define FRAME_TARGET_TIME (1000 / FPS)	// In miliseconds how long is each frame going to take: 1000/FPS
+#define FRAME_TARGET_TIME (1000 / FPS)
 
 /***************************************************************
 * GLOBALS
@@ -47,10 +45,10 @@ bool changeMap = false;
 Uint8 speed = 2;
 
 /***************************************************************
-* BUFFERS FOR MAPS
+* BUFFERS FÖR KARTOR
 ****************************************************************/
-Uint8* heightmap = NULL;	// Buffer/array to hold height values (1024/1024)
-Uint8* colormap = NULL;	// Buffer/array to hold color values  (1024/1024)
+Uint8* heightmap = NULL;
+Uint8* colormap = NULL;
 SDL_Surface* mapColor;
 SDL_Surface* mapHeight;
 struct
@@ -58,24 +56,24 @@ struct
 	Uint8 r = 0;
 	Uint8 g = 0;
 	Uint8 b = 0;
-} pixelColor;	  // r, g, b = 0
+} pixelColor;
 
 /***************************************************************
-* CAMERA
+* KAMERA
 ****************************************************************/
 struct
 {
-	float x = CAM_POS_X;	// x position on the map
-	float y = CAM_POS_Y;	// y position on the map
-	float z = RENDER_DISTANCE;	// distance of the camera looking forward
-	float h = CAM_HEIGHT;	// Height of the camera
-	float angle = 1.5 * PI;	// Camera angle (radians, clockwise) // The same as 270�
-	float horizon = CAM_HORIZON;	// Offset of the horizon position (looking up-down)
+	float x = CAM_POS_X;
+	float y = CAM_POS_Y;
+	float z = RENDER_DISTANCE;
+	float h = CAM_HEIGHT;
+	float angle = 1.5 * PI;
+	float horizon = CAM_HORIZON;
 	float tilt = 0;
 } camera;
 
 /***************************************************************
-* INITIALIZATION
+* INIT
 ****************************************************************/
 bool Init_SDL()
 {
@@ -87,11 +85,7 @@ bool Init_SDL()
 
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
-	/*
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);	// Disables deprecated OpenGL functions (glStart(); glEnd(); etc...?)
-	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
-	*/
+	
 	window = SDL_CreateWindow(
 		title.c_str(),
 		SDL_WINDOWPOS_CENTERED,
@@ -106,16 +100,9 @@ bool Init_SDL()
 		std::cerr << "Error creating SDL Window." << std::endl;
 		return false;
 	}
-	/*
-	renderer = SDL_CreateRenderer(window, -1, 0);
-	if (!renderer)
-	{
-		std::cerr << "Error creating SDL Renderer." << std::endl;
-		return false;
-	}
-	*/
+	
 	//////////////////////////////////////////////////////////////////////////////
-	//// OpenGL Initialization 
+	//// OpenGL Init 
 	//////////////////////////////////////////////////////////////////////////////
 	glContext = SDL_GL_CreateContext(window);
 	if (glContext == nullptr)
@@ -124,7 +111,7 @@ bool Init_SDL()
 		return false;
 	}
 
-	//Initialize the Glad Library
+	// Init Glad Library
 	if (!gladLoadGLLoader(SDL_GL_GetProcAddress))
 	{
 		std::cerr << "Error initializing Glad Library." << std::endl;
@@ -133,10 +120,10 @@ bool Init_SDL()
 
 	GLenum error = GL_NO_ERROR;
 
-	//Initialize Projection Matrix
+	// Init Projection Matrix
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	glOrtho(0.0, WINDOW_W, WINDOW_H, 0.0, 1.0, -1.0); // Change OpenGL coordinates from relative (-1.0 to 1.0 -> 0px to WINDOW_W/H.px)
+	glOrtho(0.0, WINDOW_W, WINDOW_H, 0.0, 1.0, -1.0);
 	error = glGetError();
 	if (error != GL_NO_ERROR)
 	{
@@ -144,7 +131,7 @@ bool Init_SDL()
 		return false;
 	}
 
-	//Initialize Modelview Matrix
+	// Init Modelview Matrix
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 	error = glGetError();
@@ -154,7 +141,7 @@ bool Init_SDL()
 		return false;
 	}
 
-	//Initialize clear color
+	// Init clear color
 	glClearColor(0.4f, 0.6f, 1.f, 1.f);
 	error = glGetError();
 	if (error != GL_NO_ERROR)
@@ -164,7 +151,7 @@ bool Init_SDL()
 	}
 	//////////////////////////////////////////////////////////////////////////////
 
-	// SDL_image init
+	// Init SDL_image
 	int imgFlags = IMG_INIT_JPG | IMG_INIT_PNG;
 	if (!(IMG_Init(imgFlags) & imgFlags))
 	{
@@ -172,7 +159,7 @@ bool Init_SDL()
 		return false;
 	}
 
-	// Trap mouse
+	// Fånga muspekaren
 	SDL_SetRelativeMouseMode(SDL_TRUE);
 
 	return true;
@@ -201,13 +188,11 @@ void UpdateTitle()
 		str3 = "";
 
 	std::string finalStr = title + str1 + str2 + str3;
-
-	//std::string finalStr = title + str1 + str2;
 	SDL_SetWindowTitle(window, finalStr.c_str());
 }
 
 /***************************************************************
-* MAPS
+* KARTOR
 ****************************************************************/
 const char* MAPS[62] = {
 	"gif/map0.color.gif",
@@ -499,8 +484,6 @@ void Input()
 			camera.x += cos(camera.angle) * delta_time * 0.1 * 4;
 			camera.y += sin(camera.angle) * delta_time * 0.1 * 4;
 		}
-
-		//std::cout << "X: " << camera.x << "\tY: " << camera.y << "\n";
 	}
 	if (keystates[SDL_SCANCODE_S] || keystates[SDL_SCANCODE_DOWN]) // Move backward 
 	{
@@ -537,7 +520,6 @@ void Input()
 			camera.x -= cos(camera.angle + 1.6) * delta_time * 0.1 * 4;
 			camera.y -= sin(camera.angle + 1.6) * delta_time * 0.1 * 4;
 		}
-		//camera.angle -= 0.1; // Turn left
 	}
 	if (keystates[SDL_SCANCODE_D] || keystates[SDL_SCANCODE_RIGHT]) // Move Right 
 	{
@@ -656,24 +638,18 @@ void Input()
 void Update()
 {
 	delta_time = (SDL_GetTicks() - lastFrameTime);
-	// Comment out if you don't want to cap the fps
-	// Calculate how much we have to wait until we reach the target frame time
 	int timeToWait = FRAME_TARGET_TIME - delta_time;
 
-	// Only Delay if we are too fast too update this frame
 	if (timeToWait > 0 && timeToWait <= FRAME_TARGET_TIME)
 	{
-		SDL_Delay(timeToWait);	// Sleep until we reach the frame target time
+		SDL_Delay(timeToWait);
 	}
-
-	// Get delta_time factor converted to seconds to be used to update objects
-	//float delta_time = (SDL_GetTicks() - lastFrameTime) / 1000.0F;
 
 	if (loopMaps)
 	{
 		timer = (int)(SDL_GetTicks() / 1000);
 
-		if ((timer % 10) == 0)	// Change map every 5 seconds
+		if ((timer % 10) == 0)
 		{
 			if (!changeMap)
 			{
@@ -693,88 +669,13 @@ void Update()
 		}
 	}
 
-	// Store milliseconds of the current frame to be used in the next one
 	lastFrameTime = SDL_GetTicks();
-	// (int)(1000.0F / (FRAME_TARGET_TIME - delta_time)) <- FPS
 }
 
 /***************************************************************
 * RENDERING
 ****************************************************************/
-/*
-void Render()
-{
-	//SDL_SetRenderDrawColor(renderer, 25, 75, 150, 255);	// Blue sky
-	//SDL_RenderClear(renderer);
-	//SDL_SetRenderDrawColor(renderer, 75, 0, 0, 255);
-	//SDL_SetRenderDrawColor(renderer, 25, 25, 25, 255);
 
-	float sinAngle = sin(camera.angle);
-	float cosAngle = cos(camera.angle);
-
-	// Left-most point of the FOV
-	float plx = cosAngle * camera.z + sinAngle * camera.z;	// pl = Pont Left
-	float ply = sinAngle * camera.z - cosAngle * camera.z;	// originally: +camera.z
-
-	// Right-most point of the FOV
-	float prx = cosAngle * camera.z - sinAngle * camera.z;	// pr = Pont Right
-	float pry = sinAngle * camera.z + cosAngle * camera.z;
-
-	// Loop WINDOW_W rays from left to right
-	for (int x = 0; x < WINDOW_W; x++)
-	{
-		float delta_x = (plx + (prx - plx) / WINDOW_W * x) / camera.z;
-		float delta_y = (ply + (pry - ply) / WINDOW_W * x) / camera.z;
-
-		// Ray (x,y) coords
-		float rayX = camera.x;
-		float rayY = camera.y;
-
-		// Collision detection
-		int collisionOffset = ((MAP_N * ((int)(rayY) & (MAP_N - 1))) + ((int)(rayX) & (MAP_N - 1)));
-		// camera.h = ((int)heightmap[collisionOffset] + 10); // walk
-		if (((int)heightmap[collisionOffset] + 10) > camera.h)
-		{
-			camera.h = ((int)heightmap[collisionOffset] + 10);
-		}
-
-		// Store the tallest projected height per-ray
-		float maxHeight = WINDOW_H;
-
-		// Loop all depth units until the zfar distance limit
-		for (int z = 1; z < camera.z; z++)
-		{
-			rayX += delta_x;
-			rayY += delta_y;
-
-			// Find the offset that we have to go and fetch values from the heightmap
-			//int mapOffset = (int)((1024 * ((int)rayY & 1023)) + ((int)rayX & 1023));	// Bitwise '&' clamps the value between 0 - 1023 = infinite
-			int mapOffset = ((MAP_N * ((int)(rayY) & (MAP_N - 1))) + ((int)(rayX) & (MAP_N - 1)));
-
-			// Project height values and find the height on-screen	// Projection: (Pos - Offset / z * Scale factor + horizon offset
-			int heightOnScreen = (int)((camera.h - heightmap[mapOffset]) / z * SCALE_FACTOR + camera.horizon);
-
-			// Only draw pixels if the new projected height is taller than the previous tallest height (maxHeight)
-			if (heightOnScreen < maxHeight)
-			{
-				// Draw pixels from previous maxHeight until the new projected height
-				for (int y = heightOnScreen; y < maxHeight; y++)
-				{
-					// draw		      width * y = row, i = x -> screen coordinates
-					//framebuffer[(WINDOW_W * y) + i] = (uint8_t)colormap[mapOffset];
-					//currentPixel = GetPixelColor(mapColor, rayX, rayY);
-
-					SDL_GetRGB(colormap[mapOffset], mapColor->format, &pixelColor.r, &pixelColor.g, &pixelColor.b);
-					//SDL_SetRenderDrawColor(renderer, pixelColor.r, pixelColor.g, pixelColor.b, SDL_ALPHA_OPAQUE);
-					//SDL_RenderDrawPoint(renderer, x, y);
-				}
-				maxHeight = heightOnScreen;
-			}
-		}
-	}
-	//SDL_RenderPresent(renderer);
-}
-*/
 void RenderGL()
 {
 	glClear(GL_COLOR_BUFFER_BIT);
@@ -782,15 +683,12 @@ void RenderGL()
 	float sinAngle = sin(camera.angle);
 	float cosAngle = cos(camera.angle);
 
-	// Left-most point of the FOV
-	float plx = cosAngle * camera.z + sinAngle * camera.z;	// pl = Pont Left
+	float plx = cosAngle * camera.z + sinAngle * camera.z;	// pl = Point Left
 	float ply = sinAngle * camera.z - cosAngle * camera.z;
 
-	// Right-most point of the FOV
-	float prx = cosAngle * camera.z - sinAngle * camera.z;	// pr = Pont Right
+	float prx = cosAngle * camera.z - sinAngle * camera.z;	// pr = Point Right
 	float pry = sinAngle * camera.z + cosAngle * camera.z;
 
-	// Loop WINDOW_W rays from left to right
 	for (int x = 0; x < WINDOW_W; x++)
 	{
 		float delta_x = (plx + (prx - plx) / WINDOW_W * x) / camera.z;
@@ -804,7 +702,7 @@ void RenderGL()
 		int collisionOffset = ((MAP_N * ((int)(rayY) & (MAP_N - 1))) + ((int)(rayX) & (MAP_N - 1)));
 
 		if (walk)
-			camera.h = ((int)heightmap[collisionOffset] + 10); // lock camera to ground/walk
+			camera.h = ((int)heightmap[collisionOffset] + 10); // Fäst kameran mot marken / walk
 
 		if (camera.h < ((int)heightmap[collisionOffset] + 10))
 		{
@@ -813,36 +711,32 @@ void RenderGL()
 
 		if (loopMaps)
 		{
-			int collisionFront = ((MAP_N * ((int)(rayY + (sin(camera.angle) * 25)) & (MAP_N - 1))) + ((int)(rayX + (cos(camera.angle) * 25)) & (MAP_N - 1)));	// Height in Front of camera
+			int collisionFront = ((MAP_N * ((int)(rayY + (sin(camera.angle) * 25)) & (MAP_N - 1))) + ((int)(rayX + (cos(camera.angle) * 25)) & (MAP_N - 1)));
 
 			if (camera.h < ((int)heightmap[collisionFront] + 10))
 			{
-				int collisionRight = ((MAP_N * ((int)(rayY + (sin(camera.angle + 1) * 50)) & (MAP_N - 1))) + ((int)(rayX + (cos(camera.angle + 1) * 50)) & (MAP_N - 1)));	// Height to Right (angle + 1)
-				int collisionLeft = ((MAP_N * ((int)(rayY + (sin(camera.angle - 1) * 50)) & (MAP_N - 1))) + ((int)(rayX + (cos(camera.angle - 1) * 50)) & (MAP_N - 1)));	// Height to Left  (angle - 1)
+				int collisionRight = ((MAP_N * ((int)(rayY + (sin(camera.angle + 1) * 50)) & (MAP_N - 1))) + ((int)(rayX + (cos(camera.angle + 1) * 50)) & (MAP_N - 1)));	// Höjd till höger (angle + 1)
+				int collisionLeft = ((MAP_N * ((int)(rayY + (sin(camera.angle - 1) * 50)) & (MAP_N - 1))) + ((int)(rayX + (cos(camera.angle - 1) * 50)) & (MAP_N - 1)));	// Höjd till vänster  (angle - 1)
 
-				if (camera.h > ((int)heightmap[collisionRight]) && camera.h < ((int)heightmap[collisionLeft]))	// Height lower to the Right
-					camera.angle += 0.000005 * delta_time;	// Turn Right
-				if (camera.h < ((int)heightmap[collisionRight]) && camera.h >((int)heightmap[collisionLeft]))	// Height lower to the Left
-					camera.angle -= 0.000005 * delta_time;	// Turn Left
+				if (camera.h > ((int)heightmap[collisionRight]) && camera.h < ((int)heightmap[collisionLeft]))
+					camera.angle += 0.000005 * delta_time;
+				if (camera.h < ((int)heightmap[collisionRight]) && camera.h >((int)heightmap[collisionLeft]))
+					camera.angle -= 0.000005 * delta_time;
 			}
 		}
 
-		// Store the tallest projected height per-ray
 		float maxHeight = WINDOW_H + 1;
 
-		// Loop all depth units until the zfar distance limit
 		for (int z = 1; z < camera.z; z++)
 		{
 			rayX += delta_x;
 			rayY += delta_y;
 
-			// Find the offset that we have to go and fetch values from the heightmap - Bitwise '&' clamps the value between 0 - 1023 = world repeats
+			// Offset för att hämta rätt värden från heightmap, bitwise '&' håller värdet mellan 0 - 1023 = kartan upprepas
 			int mapOffset = ((MAP_N * ((int)(rayY) & (MAP_N - 1))) + ((int)(rayX) & (MAP_N - 1)));
-
-			// Project height values and find the height on-screen	// Projection: (Pos - Offset / z * Scale factor + horizon offset
+			
 			int heightOnScreen = (int)((camera.h - heightmap[mapOffset]) / z * SCALE_FACTOR + camera.horizon);
 
-			// Only draw pixels if the new projected height is taller than the previous tallest height (maxHeight)
 			if (heightOnScreen < maxHeight)
 			{
 				if (loopMaps)
@@ -864,7 +758,6 @@ void RenderGL()
 				}
 				else
 				{
-					// Draw pixels from previous maxHeight until the new projected height
 					for (int y = heightOnScreen; y < maxHeight; y++)
 					{
 						SDL_GetRGB(colormap[mapOffset], mapColor->format, &pixelColor.r, &pixelColor.g, &pixelColor.b);
@@ -879,105 +772,10 @@ void RenderGL()
 			}
 		}
 	}
-	/*
-	glBegin(GL_LINES);
-		glColor3ub(0, 255, 0);
-		// Top
-		glVertex2i(WINDOW_W / 2, WINDOW_H / 2 - 5);
-		glVertex2i(WINDOW_W / 2, WINDOW_H / 2 - 15);
-		// Bottom
-		glVertex2i(WINDOW_W / 2, WINDOW_H / 2 + 5);
-		glVertex2i(WINDOW_W / 2, WINDOW_H / 2 + 15);
-		// Left
-		glVertex2i(WINDOW_W / 2 - 5, WINDOW_H / 2);
-		glVertex2i(WINDOW_W / 2 - 15, WINDOW_H / 2);
-		// Right
-		glVertex2i(WINDOW_W / 2 + 5, WINDOW_H / 2);
-		glVertex2i(WINDOW_W / 2 + 15, WINDOW_H / 2);
-	glEnd();
-
-	for (int mX = 0; mX < 1024; mX++)
-	{
-		for (int mY = 0; mY < 1024; mY++)
-		{
-			int mMapOffset = (1024 * mY + mX);
-			SDL_GetRGB(colormap[mMapOffset], mapColor->format, &pixelColor.r, &pixelColor.g, &pixelColor.b);
-			glBegin(GL_POINTS);
-				glColor3ub(pixelColor.r, pixelColor.g, pixelColor.b);
-				glVertex2i(mX/6, mY/6);
-			glEnd();
-		}
-	}
-	*/
+	
 	SDL_GL_SwapWindow(window);
 }
-/*
-void RenderGL2()
-{
-	glClear(GL_COLOR_BUFFER_BIT);
 
-	float sinAngle = sin(camera.angle);
-	float cosAngle = cos(camera.angle);
-
-	float maxHeight = WINDOW_H;
-
-	float delta_z = 1.0F;
-
-	for (int z = 1; z < camera.z; z++)
-	{
-		float plx = -cosAngle * delta_z - sinAngle * delta_z;
-		float ply =  sinAngle * delta_z - cosAngle * delta_z;
-		float prx =  cosAngle * delta_z - sinAngle * delta_z;
-		float pry = -sinAngle * delta_z - cosAngle * delta_z;
-
-		float delta_x = (prx - plx) / WINDOW_W;
-		float delta_y = (pry - ply) / WINDOW_W;
-
-		plx += camera.x;
-		ply += camera.y;
-
-		float invz = 1.0F / delta_z * 240;
-
-		for (int x = 0; x < WINDOW_W; x++)
-		{
-			int mapOffset = ((((int)(ply) & (MAP_N - 1)) << 10) + ((int)(plx) & (MAP_N - 1)));
-			int heightOnScreen = (int)((camera.h - heightmap[mapOffset]) * invz + camera.horizon);
-
-			if (heightOnScreen < 0)
-			{
-				heightOnScreen = 0;
-			}
-
-			if (heightOnScreen > maxHeight)
-			{
-				return;
-			}
-
-			//int offset = ((heightOnScreen * WINDOW_H) + x);
-			for (int y = heightOnScreen; y < maxHeight; y++)
-			{
-				SDL_GetRGB(colormap[mapOffset], mapColor->format, &pixelColor.r, &pixelColor.g, &pixelColor.b);
-
-				glBegin(GL_POINTS);
-				glColor3ub(pixelColor.r, pixelColor.g, pixelColor.b);
-				glVertex2i(x, y);
-				glEnd();
-			}
-
-			if (heightOnScreen < maxHeight)
-			{
-				maxHeight = heightOnScreen;
-			}
-
-			plx += delta_x;
-			ply += delta_y;
-		}
-		delta_z += 0.005;
-	}
-
-	SDL_GL_SwapWindow(window);
-}
-*/
 /***************************************************************
 * DEMO
 ****************************************************************/
@@ -987,9 +785,9 @@ void Demo()
 	{
 		if (speed == 1)
 		{
-			camera.h = (sin(counter) * 100) + 175;	// Up/Down movement
+			camera.h = (sin(counter) * 100) + 175;
 			if (walk)
-				camera.horizon = CAM_HORIZON;		// Horizon
+				camera.horizon = CAM_HORIZON;
 			else
 				camera.horizon = -1 * ((sin(counter - 1) * 100) + 200) + 200;
 			if ((20 - (timer % 20)) < 5)
@@ -1012,9 +810,9 @@ void Demo()
 		}
 		else if (speed == 2)
 		{
-			camera.h = (sin(counter) * 100) + 175;	// Up/Down movement
+			camera.h = (sin(counter) * 100) + 175;
 			if (walk)
-				camera.horizon = CAM_HORIZON;		// Horizon
+				camera.horizon = CAM_HORIZON;
 			else
 				camera.horizon = -1 * ((sin(counter - 1) * 100) + 200) + 200;
 			if ((20 - (timer % 20)) < 5)
@@ -1037,9 +835,9 @@ void Demo()
 		}
 		else if (speed == 3)
 		{
-			camera.h = (sin(counter) * 100) + 175;	// Up/Down movement
+			camera.h = (sin(counter) * 100) + 175;
 			if (walk)
-				camera.horizon = CAM_HORIZON;			// Horizon
+				camera.horizon = CAM_HORIZON;
 			else
 				camera.horizon = -1 * ((sin(counter - 1) * 100) + 200) + 200;
 			if ((20 - (timer % 20)) < 5)
@@ -1062,16 +860,6 @@ void Demo()
 		}
 
 		counter += 0.02;
-
-		//std::cout << " tilt: " << (float)camera.tilt << "\n";
-		/*
-		std::cout << " Hz: " << (int)camera.horizon;
-		std::cout << " H: " << (int)camera.h;
-		std::cout << " X: " << (int)camera.x;
-		std::cout << " Y: " << (int)camera.y;
-		std::cout << " A: " << (float)camera.angle;
-		std::cout << "\n";
-		*/
 	}
 }
 
@@ -1099,17 +887,6 @@ int main(int argc, char* argv[])
 
 	LoadMap(currentMap * MAP_SHIFT);
 
-	// DEBUG TESTING
-	//currentPixel = GetPixelColor(mapColor, 0, 0);
-	//std::cout << "R: " << (int)currentPixel.r << " G: " << (int)currentPixel.g << " B: " << (int)currentPixel.b << " A: " << (int)currentPixel.a << "\n";
-	//std::cout << mapHeight->w << std::endl;
-	/*
-	SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, mapColor);
-	SDL_RenderClear(renderer);
-	SDL_RenderCopy(renderer, texture, NULL, NULL);
-	SDL_RenderPresent(renderer);
-	*/
-
 	std::cout << "-Keyboard-\n";
 	std::cout << "W, A, S, D, < ^ v >:\tMove\n";
 	std::cout << "R, F, PgUp, PgDn:\tMove up/down\n";
@@ -1131,9 +908,7 @@ int main(int argc, char* argv[])
 		Input();
 		Update();
 		Demo();
-		//Render();
 		RenderGL();
-		//RenderGL2();
 	}
 
 	Cleanup();
